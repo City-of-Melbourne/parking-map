c3b3043d908033a64edaf977eca74f74f1a7c68c
com-TIM <tim.gray@melbourne.vic.gov.au>
2017-11-15 11:00:50 +1100

    Update index.js

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ function innerCircle(partialId, filter, color) {
         type: 'circle',
         filter: filter,
         paint: {
-            'circle-radius': { base: 1, stops: [ [13, 2], [18, 6]] },
+            'circle-radius': { base: 3, stops: [ [10, 2], [18, 6]] },
             'circle-color': color || 'white'
         }
     };
@@ -199,7 +199,7 @@ function outerCircle(partialId, filter, color) {
         type: 'circle',
         filter: filter,
         paint: {
-            'circle-radius': { base: 2, stops: [ [13, 2], [18, 10]]},
+            'circle-radius': { base: 2, stops: [ [10, 2], [18, 10]]},
             'circle-color': color
         }
     }
