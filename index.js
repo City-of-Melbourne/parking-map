800d80fb8f77e4d9f7cdf25d76030e213ac39fdf
com-TIM <tim.gray@melbourne.vic.gov.au>
2017-11-15 14:36:47 +1100

    Update index.js

--- index.js
+++ index.js
@@ -285,7 +285,7 @@ function addLayers(disabilityPermit) {
 
     // we add separate layers so that the not-presents can be on top
     // map.addLayer(sensorLayer('present',     ['==', 'status','Present'],     'hsl(350, 90%, 40%)'));
-    var colorPresent = '#000000';//'hsl(350, 90%, 0%)';
+//    var colorPresent = '#000000';//'hsl(350, 90%, 0%)';
     var colorNotPresent = 'white';
     var colorInactive = '#bbb';
     var colorOuter = '#555'; // very thin outline outside the thick band
@@ -312,12 +312,12 @@ function addLayers(disabilityPermit) {
         */
 
    
- addLayer(outerCircle('disabled-outer', ['==', 'Exemption','Disabled Logo'], colorDisabled)); // TODO avoid overlapping with previous
+
     
     //addLayer(innerCircle('not-present', ['==', 'status','Not Present'], 'hsl(100, 90%, 60%)'));
     addLayer(outerCircle('present', ['==', 'status','Present'], colorPresent));
     addLayer(outerCircle('not-present', ['all', ['==', 'status','Unoccupied'], ['!=', 'TypeDesc', 'Loading Zone']], colorRestrictions));
-    //addLayer(outerCircle('disabled-outer', ['==', 'Exemption','Disabled Logo'], colorDisabled)); // TODO avoid overlapping with previous
+    addLayer(outerCircle('disabled-outer', ['==', 'Exemption','Disabled Logo'], colorDisabled)); // TODO avoid overlapping with previous
     
     /*addLayer(innerCircle('loading-zone', ['==', 'TypeDesc', 'Loading Zone'], 'white'));
     addLayer(innerCircle('disabled', ['==', 'Exemption','Disabled Logo'], colorDisabled));*/
