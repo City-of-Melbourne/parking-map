280db330620f2b01f69340b603db285f18128338
com-TIM <tim.gray@melbourne.vic.gov.au>
2017-11-17 11:45:51 +1100

    Update index.js

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ function addLayers(disabilityPermit) {
     var colorNotPresent = 'white';
     var colorInactive = '#F2F3F4';
     var colorOuter = '#555'; // very thin outline outside the thick band
-    var colorDisabled = disabilityPermit ? '#645FAA' : '#D4D6DB'; //'hsl(230,95%,70%)';
+    var colorDisabled = disabilityPermit ? '#FF0000' : '#D4D6DB'; //'hsl(230,95%,70%)';
 
     var colorRestrictions = {
         property: disabilityPermit ? 'DisabilityExt' : 'Duration' ,
